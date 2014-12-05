82335f17f992da70d76af3ffa3c1b5277aa23245
Danny <dannylshaw@yahoo.com>
2014-12-05 18:15:41 -0500

    leonardo da vinci

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leonardo::Application.config.secret_token = '7b61a0367fa8f9e679f4f19e1250d6bc2367eac3c64d468ee1ca5e34dfaaaf9941cda6413fe99098b5d074bb66d013d9461aacd2fb6b55e90758e9f0fdf148f6'
